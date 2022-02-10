6019553c13488c8b969937f8db2e00897bebc09d
Saatvik1808 <saatvik.shrivastava08@gmail.com>
2022-02-10 21:54:09 +0530

    stockss

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Chart from "./Components/Chart";
+import Chart2 from "./Components/Chart2";
+import Chart3 from "./Components/Chart3";
+import Chart4 from "./Components/Chart4";
+import Blue from "./Blue.mp4";
+import Typewriter from "typewriter-effect";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        <div className="saatvik">
+          <Typewriter
+            onInit={(typewriter) => {
+              typewriter.typeString("STOCK MARKET ANALYZER").start();
+            }}
+          />
+        </div>
+      </h1>
+      <video
+        autoPlay
+        loop
+        muted
+        style={{
+          position: "fixed",
+          width: "100%",
+          left: "50%",
+          top: "50%",
+          height: "100%",
+          objectFit: "cover",
+          transform: "translate(-50%,-50%)",
+          zIndex: "-1",
+        }}
+      >
+        <source src={Blue} type="video/mp4" />
+      </video>
+      <Chart />
+      <Chart2 />
+      <Chart3 />
+      <Chart4 />
     </div>
   );
 }
---
+++ src/Components/Chart4.js
@@ -0,0 +1,79 @@
+import React from "react";
+import { Bar, Line } from "react-chartjs-2";
+import { Chart as ChartJS } from "chart.js/auto";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./Chart.css";
+
+const options = {
+  scales: {
+    x2: {
+      position: "bottom",
+      reverse: true,
+    },
+  },
+
+  responsive: true,
+
+  plugins: {
+    legend: {
+      position: "top",
+    },
+    title: {
+      display: true,
+    },
+  },
+};
+
+const x = [];
+const y = [];
+
+const Chart4 = () => {
+  const [stock, setStock] = useState([]);
+  useEffect(() => {
+    const API_KEY = "XIZTSWJ40Z3XZ6F3";
+    axios
+      .get(
+        `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=TSCO.LON&outputsize=compact&apikey=${API_KEY}`
+      )
+      .then((res) => {
+        console.log(res);
+        setStock(res.data);
+        for (var key in res.data["Time Series (Daily)"]) {
+          y.push(key);
+          x.push(res.data["Time Series (Daily)"][key]["1. open"]);
+        }
+        console.log("x", x);
+        console.log("y", y);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  const data = {
+    labels: y,
+    datasets: [
+      {
+        label: "Tesco PLC (UK - London Stock Exchange)",
+        data: x,
+        fill: true,
+
+        borderJoinStyle: "miter",
+        backgroundColor: "rgba(25, 299, 232, 0.2)",
+        padding: "0px",
+
+        borderColor: "black",
+        xAxisID: "x2",
+      },
+    ],
+  };
+
+  return (
+    <div className="chart" style={{ width: "80%" }}>
+      <Line data={data} options={options} />
+    </div>
+  );
+};
+
+export default Chart4;
